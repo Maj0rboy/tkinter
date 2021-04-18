e053eff5f1de97d09d7e512706c743b07ce07735
ahmed <ahmed2018@students.uonbi.ac.ke>
2021-04-18 23:46:14 +0300

    Add files via upload

---
+++ radiobtton.py
@@ -0,0 +1,30 @@
+from tkinter import *
+root = Tk()
+# r = IntVar()
+# r.set("2")
+
+toppings = [
+    ("Pepperoni", "Pepperoni"),
+    ("cheese", "cheese"), 
+    ("mushroom", "mushroom"),
+    ("onion", "onion")
+    
+]
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, topping in toppings:
+    Radiobutton(root, text=text, variable= pizza, value= topping, anchor=W).pack()
+
+def clickme(value):
+    mylabel = Label(root, text= value)
+    mylabel.pack()
+    
+# Radiobutton(root, text="option 1", variable= r, value=1, command=lambda: clickme(r.get())).pack()
+# Radiobutton(root, text="option 2", variable= r, value=2, command=lambda: clickme(r.get())).pack()
+mybutton = Button(root, text="click me", command= lambda: clickme(pizza.get())).pack()
+
+# mylabel = Label(root, text= r.get())
+# mylabel.pack()
+
+root.mainloop()
