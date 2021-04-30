b06210563f0662542ee2fe66665b7e2863049f79
ahmed <ahmed2018@students.uonbi.ac.ke>
2021-04-30 22:59:40 +0300

    Update slider.py

--- slider.py
+++ slider.py
@@ -5,7 +5,7 @@ root.geometry("400x400")
 vertical = Scale(root, from_ = 0, to=200)
 vertical.pack()
 
-horizontal = Scale(root, from_ = 0, to = 200, orient=HORIZONTAL)
+horizontal = Scale(root, from_ = 0, to = 250, orient=HORIZONTAL)
 horizontal.pack()
 
 mainloop()
